14da769a39ed8e810ea6506fea7b281b345095e2
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-04-01 02:32:05 +0500

    basic material-ui theme setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Typography } from '@material-ui/core';
+import Hidden from '@material-ui/core/Hidden';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Typography>hello</Typography>
+			</div>
+		);
+	}
+}
+
+export default App;
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { dark } from '@material-ui/core/styles/createPalette';
+
+const theme = createMuiTheme({
+	palette: {
+		type: 'dark'
+	}
+});
+
+ReactDOM.render(
+	<React.Fragment>
+		<MuiThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</MuiThemeProvider>
+	</React.Fragment>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
