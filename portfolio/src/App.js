3d5fa2049b5701e36a675075c9ae07ae014d5489
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-04-01 06:12:21 +0500

    added links and refactored styles into const

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import { Typography, Avatar } from '@material-ui/core';
 import Hidden from '@material-ui/core/Hidden';
+import ScrollspyNav from 'react-scrollspy-nav';
 
-import { borders } from '@material-ui/system';
+const styles = {
+	sidePane: {
+		backgroundColor: '#292828',
+		position: 'fixed',
+		top: 0,
+		left: 0,
+		display: 'flex',
+		flexDirection: 'column',
+		width: '12rem',
+		height: '100vh'
+	},
+	sidePaneAvatar: {
+		width: '92px',
+		height: '92px',
+		marginTop: '4rem',
+		border: 'solid 0.3rem #16a422'
+	},
+	sidePaneLink: {}
+};
 
 class App extends Component {
 	render() {
@@ -10,39 +29,48 @@ class App extends Component {
 			<div className="App">
 				{/* Desktop View */}
 				<Hidden xsDown>
-					<Typography component="h3" variant="h3">
-						{/* Side Pane */}
+					{/* Side Pane */}
+					<div style={styles.sidePane}>
 						<div
 							style={{
-								backgroundColor: '#292828',
-								position: 'fixed',
-								top: 0,
-								left: 0,
-								display: 'flex',
-								flexDirection: 'column',
-								width: '12rem',
-								height: '100vh'
+								marginLeft: 'auto',
+								marginRight: 'auto'
 							}}
 						>
-							<div
-								style={{
-									marginLeft: 'auto',
-									marginRight: 'auto'
-								}}
-							>
-								<Avatar
-									style={{
-										width: '92px',
-										height: '92px',
-										marginTop: '4rem',
-										border: 'solid 0.3rem #16a422'
-									}}
-									src="https://avatars1.githubusercontent.com/u/42393962?s=460&v=4"
-									alt="Saud Tauqeer Profile image"
-								/>
-							</div>
+							{/* Profile image */}
+							<Avatar
+								style={styles.sidePaneAvatar}
+								src="https://avatars1.githubusercontent.com/u/42393962?s=460&v=4"
+								alt="Saud Tauqeer Profile image"
+							/>
 						</div>
-					</Typography>
+						<div>
+							<ScrollspyNav
+								scrollTargetIds={[ 'about', 'skills', 'experience', 'projects', 'learning' ]}
+								activeNavClass="is-active"
+							>
+								<a style={styles.sidePaneLink} href="#about">
+									<Typography>About</Typography>
+								</a>
+
+								<a style={styles.sidePaneLink} href="#skills">
+									Skills
+								</a>
+
+								<a style={styles.sidePaneLink} href="#experience">
+									Experience
+								</a>
+
+								<a style={styles.sidePaneLink} href="#projects">
+									Projects
+								</a>
+
+								<a style={styles.sidePaneLink} href="#learning">
+									learning
+								</a>
+							</ScrollspyNav>
+						</div>
+					</div>
 				</Hidden>
 
 				{/* Mobile view */}
